30555870844457309490507e0bfcb8f4a19f2b66
Meow <james.liu841@gmail.com>
2018-05-17 16:55:51 +0800

    Daily Update

--- 1009.py
+++ 1009.py
@@ -1,7 +1,12 @@
-from datetime import datetime, timedelta
-t1 = datetime(2000, 1, 1, *[int(x) for x in input().split(':')])
-t2 = datetime(2000, 1, 1, *[int(x) for x in input().split(':')])
-t3 = timedelta(days=1)
-if t2 < t1:
-    t2 += t3
-print(t2 - t1)
+h1, m1, s1 = [int(x) for x in input().split(':')]
+h2, m2, s2 = [int(x) for x in input().split(':')]
+h2 += 24
+s2, m2, h2 = s2-s1, m2-m1, h2-h1
+if s2 < 0:
+    s2 += 60
+    m2 -= 1
+if m2 < 0:
+    m2 += 60
+    h2 -= 1
+h2 %= 24
+print('{:02d}:{:02d}:{:02d}'.format(h2,m2,s2))
