646a854577547d8e0d05b17c7b8beaf1f6857536
Meow <james.liu841@gmail.com>
2018-05-30 11:12:00 +0800

    Fix some bugs

--- 1056.py
+++ 1056.py
@@ -17,31 +17,38 @@ def check_matrix(mat):
         O_win += 1
     if mat[::3].count('X') == 3:
         X_win += 1
+    if mat[1::3].count('O') == 3:
+        O_win += 1
+    if mat[1::3].count('X') == 3:
+        X_win += 1
     if mat[2::3].count('O') == 3:
         O_win += 1
     if mat[2::3].count('X') == 3:
-        X_win += 1
-    if mat[3::3].count('O') == 3:
-        O_win += 1
-    if mat[3::3].count('X') == 3:
         X_win += 1
     if mat[::4].count('O') == 3:
         O_win += 1
     if mat[::4].count('X') == 3:
         X_win += 1
-    if mat[3:8:2].count('O') == 3:
+    if mat[2:8:2].count('O') == 3:
         O_win += 1
-    if mat[3:8:3].count('X') == 3:
+    if mat[2:8:2].count('X') == 3:
         X_win += 1
     return O_win, X_win
 matrix = list(input()) + list(input()) + list(input())
+for x in matrix:
+    if not (x == 'O' or x == 'X' or x == '.'):
+        print('IMPOSSIBLE')
+        exit()
 a = matrix.count('O')
 b = matrix.count('X')
-if a-b == 1 or a-b == 0:
+if a-b == 1:
     O, X = check_matrix(matrix)
-    if O + X > 1:
-        print('IMPOSSIBLE')
-    print('POSSIBLE')
-else:
-    print('IMPOSSIBLE')
-print(matrix.count('.'))
+    if (X == 0 and (O == 0 or O == 1 or O == 2)):
+        print('POSSIBLE')
+        exit()
+elif a-b == 0:
+    O, X = check_matrix(matrix)
+    if (O == 0 and (X == 0 or X == 1 or X == 2)):
+        print('POSSIBLE')
+        exit()
+print('IMPOSSIBLE')
