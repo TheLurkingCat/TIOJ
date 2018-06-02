9d4a0561b69a5a6d69644a233f982189b3749cc1
meow <james.liu841@gmail.com>
2018-06-02 14:18:06 +0800

    fix

--- 1017.cpp
+++ 1017.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <cstdio>
 
-using namespace std
+using namespace std;
 
-int T, N
-int S[2000000], min_value[2000000], max_value[2000000]
+int T, N;
+int S[2000000], min_value[2000000], max_value[2000000];
 
 int main()
 {
-    scanf("%d", & T)
+    scanf("%d", & T);
     while(T--)
     {
-        int ans = 0
-        scanf("%d", & N)
+        int ans = 0;
+        scanf("%d", & N);
         for(int i=0; i < N; i++)
-        scanf("%d", & S[i])
-        max_value[0] = S[0]
+        scanf("%d", & S[i]);
+        max_value[0] = S[0];
 
         for(int i=1; i < N; i++)
-        max_value[i] = max(max_value[i-1], S[i])
+        max_value[i] = max(max_value[i-1], S[i]);
 
-        min_value[N-1] = S[N-1]
+        min_value[N-1] = S[N-1];
 
         for(int i=N-2; i >= 0; i--)
-        min_value[i] = min(min_value[i+1], S[i])
+        min_value[i] = min(min_value[i+1], S[i]);
 
-        for(int i=1
-            i < N-1
+        for(int i=1;
+            i < N-1;
             i++)
-        if(S[i] > max_value[i-1] & & S[i] < min_value[i+1]) ans++
+        if(S[i] > max_value[i-1] && S[i] < min_value[i+1]) ans++;
 
-        printf("%d\n", ans)
+        printf("%d\n", ans);
     }
 }
