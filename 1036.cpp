732f100657a5e4c2e17ef69b71acc760c2f11657
Meow <james.liu841@gmail.com>
2018-06-15 16:49:03 +0800

    Update

--- 1036.cpp
+++ 1036.cpp
@@ -1,18 +1,20 @@
 #include <stdint.h>
 #include <iostream>
+#define MAX 1000001
 using namespace std;
-bool prime[10000001];
-int ans[10000001];
+bool prime[MAX];
+int ans[MAX];
 int a, primes = 0, x;
 int main() {
-    fill(prime, prime + 10000001, true);
+    fill(prime, prime + MAX, true);
     ans[0] = ans[1] = 0;
     prime[0] = prime[1] = false;
-    for (int i = 2; i < 10000001; i++) {
+    for (int i = 2; i < MAX; i++) {
         ans[i] = primes + prime[i];
         if (prime[i]) {
+            cout << i << ',';
             primes++;
-            for (uint64_t j = (uint64_t)i * i; j < 10000001; j += i) {
+            for (uint64_t j = (uint64_t)i * i; j < MAX; j += i) {
                 prime[j] = false;
             }
         }
